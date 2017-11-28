e42c915ccacdc01ae7b10daeef89d1b72ed6013d
Jan Buschtöns <jan@buschtoens.me>
2017-11-28 14:57:11 +0100

    chore(linter): add linter configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,26 @@
 module.exports = {
   root: true,
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'eslint:recommended',
+  plugins: [
+    'ember',
+    'prettier'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'prettier'
+  ],
   env: {
     browser: true
   },
   rules: {
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'ember/named-functions-in-promises': ['error', { allowSimpleArrowFunction: true }],
+    'no-var': 'error',
+    'prefer-const': 'error'
   }
 };
--- testem.js
+++ testem.js
@@ -2,12 +2,8 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
-  launch_in_ci: [
-    'Chrome'
-  ],
-  launch_in_dev: [
-    'Chrome'
-  ],
+  launch_in_ci: ['Chrome'],
+  launch_in_dev: ['Chrome'],
   browser_args: {
     Chrome: {
       mode: 'ci',
@@ -17,6 +13,6 @@ module.exports = {
         '--remote-debugging-port=9222',
         '--window-size=1440,900'
       ]
-    },
+    }
   }
 };
