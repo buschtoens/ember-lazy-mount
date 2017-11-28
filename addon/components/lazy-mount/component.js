9e241ed6a0b64e784387c40dbbbfb4f14786d7fc
Jan Buschtöns <jan@buschtoens.me>
2017-11-28 16:20:46 +0100

    feat: initial implementation

---
+++ addon/components/lazy-mount/component.js
@@ -0,0 +1,54 @@
+import Component from '@ember/component';
+import { get } from '@ember/object';
+import { next } from '@ember/runloop';
+import { tagName } from 'ember-decorators/component';
+import { service } from 'ember-decorators/service';
+import { reads } from 'ember-decorators/object/computed';
+import { argument } from '@ember-decorators/argument';
+import { type } from '@ember-decorators/argument/type';
+import { task } from 'ember-concurrency';
+import layout from './template';
+
+@tagName('')
+export default class LazyMountComponent extends Component.extend({ layout }) {
+  @service engineLoader;
+
+  static positionalParams = ['name'];
+
+  @argument
+  @type('string')
+  name;
+
+  @argument
+  @type('any')
+  model = null;
+
+  @reads('loadEngine.last.value.name') loadedName;
+
+  @reads('loadEngine.isRunning') isLoading;
+
+  @reads('loadEngine.last.error') error;
+
+  didReceiveAttrs() {
+    // super.didReceiveAttrs();
+    this._super();
+
+    const name = get(this, 'name');
+    if (name !== get(this, 'loadedName')) {
+      // only load a new engine, if it is different from the last one
+      next(() => {
+        // this hack is currently required, because the task is only initialized
+        // after `didReceiveAttrs` was run for the first time
+        get(this, 'loadEngine').perform(name);
+      });
+    }
+  }
+
+  loadEngine = task(function*(name = get(this, 'name')) {
+    const engineLoader = get(this, 'engineLoader');
+    if (!engineLoader.isLoaded(name)) {
+      yield get(this, 'engineLoader').load(name);
+    }
+    return { name };
+  }).restartable();
+}
