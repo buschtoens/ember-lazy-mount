3bd06db7fdd52bf88aca7b04e54329c4f548b6be
Jan Buschtöns <jan@buschtoens.me>
2018-12-14 10:40:23 +0100

    test: make it more stable

--- tests/integration/components/lazy-mount/component-test.ts
+++ tests/integration/components/lazy-mount/component-test.ts
@@ -3,24 +3,41 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render, settled } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import Service from '@ember/service';
+import { schedule } from '@ember/runloop';
 import { timeout } from 'ember-concurrency';
 
 module('Integration | Component | lazy-mount', function(hooks) {
   setupRenderingTest(hooks);
 
   test('it works', async function(assert) {
-    await render(hbs`
+    assert.expect(2);
+
+    render(hbs`
       <LazyMount @name="an-engine" as |e|>
         isLoading: {{e.isLoading}};
         error: {{e.error}};
       </LazyMount>
     `);
 
-    assert.dom().hasText('isLoading: true; error: ;');
+    // Using `await render(...)` instead is flaky, because sometimes the request
+    // starts before the render queue is flushed. This then causes the `await`
+    // to also wait for the request to finish, meaning that we never get to test
+    // the loading state.
+    schedule('afterRender', () =>
+      assert.dom().hasText('isLoading: true; error: ;', 'shows a loading state')
+    );
 
+    // Seems like the task is somehow not correctly awaited for by `settled`.
+    // This rather is a problem upstream in ember-concurrency. Adding a
+    // sufficient timeout is not really clever, but it works reliably in CI.
+    await timeout(500); // ¯\_(ツ)_ /¯
+
+    // Wait for the template to update.
     await settled();
 
-    assert.dom().hasText('Hello from the other side.');
+    assert
+      .dom()
+      .hasText('Hello from the other side.', 'shows the engine template');
   });
 
   test('it shows an error right away for a non-existing engine', async function(assert) {
@@ -36,33 +53,44 @@ module('Integration | Component | lazy-mount', function(hooks) {
       .hasText(
         'isLoading: false; error: Error: Assertion Failed: No bundle with name "not-an-engine" exists in the asset manifest.;'
       );
-
-    await settled();
   });
 
-  test('it shows an error, if the promise rejects', async function(assert) {
+  test('it shows a loading state and an error', async function(assert) {
+    assert.expect(2);
+
     class AssetLoaderService extends Service {
       async loadBundle() {
-        await timeout(10);
+        await new Promise(resolve => schedule('afterRender', resolve));
         throw new Error('something failed');
       }
     }
     this.owner.unregister('service:asset-loader');
     this.owner.register('service:asset-loader', AssetLoaderService);
 
-    await render(hbs`
+    render(hbs`
       <LazyMount @name="an-engine" as |e|>
         isLoading: {{e.isLoading}};
         error: {{e.error}};
       </LazyMount>
     `);
 
-    // assert.dom().hasText('isLoading: true; error: ;');
+    // Using `await render(...)` instead would not work, since the timer in
+    // `loadBundle` is synchronously added to the queue in order to avoid
+    // blazing past the promise with the last assertion. The alternative would
+    // be storing the promise in the closure of the test and awaiting it further
+    // down.
+    schedule('afterRender', () =>
+      assert.dom().hasText('isLoading: true; error: ;', 'shows a loading state')
+    );
 
+    // Waits for the promise in `loadBundle` to resolve and the template to re-render.
     await settled();
 
-    assert.dom().hasText('isLoading: false; error: Error: something failed;');
-
-    await settled();
+    assert
+      .dom()
+      .hasText(
+        'isLoading: false; error: Error: something failed;',
+        'shows an error'
+      );
   });
 });
